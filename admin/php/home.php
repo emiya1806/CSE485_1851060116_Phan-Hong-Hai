f6a5c3694712308eeda5dbedee2d9e1b3a3f4242
Phan Hải <50164884+emiya1806@users.noreply.github.com>
2021-01-18 14:22:45 +0700

    Update home.php

--- admin/php/home.php
+++ admin/php/home.php
@@ -47,45 +47,45 @@
     </div>
     <div class="form-group col-md-6">
       <label for="twitter">Twitter</label>
-      <input type="text" class="form-control" value="<?=$data['twitter']?>" name="twitter" id="twitter" placeholder="https://twitter.com/MohanGo94273231">
+      <input type="text" class="form-control" value="<?=$data['twitter']?>" name="twitter" id="twitter" placeholder="https://twitter.com/k1ller_Que3n">
     </div>
     
     <div class="form-group col-md-6">
       <label for="facebook">Facebook</label>
-      <input type="text" class="form-control" value="<?=$data['facebook']?>" name="facebook" id="facebook" placeholder="https://facebook.com/MohanGo94273231">
+      <input type="text" class="form-control" value="<?=$data['facebook']?>" name="facebook" id="facebook" placeholder="https://www.facebook.com/profile.php?id=100027259371639">
     </div>
     
     <div class="form-group col-md-6">
       <label for="instagram">Instagram</label>
-      <input type="text" class="form-control" value="<?=$data['instagram']?>" name="instagram" id="instagram" placeholder="https://instagram.com/MohanGo94273231">
+      <input type="text" class="form-control" value="<?=$data['instagram']?>" name="instagram" id="instagram" placeholder="https://www.instagram.com/k1llerque3n/">
     </div>
     
     <div class="form-group col-md-6">
-      <label for="skype">Skype</label>
-      <input type="text" value="<?=$data['skype']?>" class="form-control" name="skype" id="skype" placeholder="@monu250">
+      <label for="reddit">Skype</label>
+      <input type="text" class="form-control" value="<?=$data['reddit']?>" name="reddit" id="reddit" placeholder="https://www.reddit.com/u/k1ller_Que3n">
     </div>
     
     <div class="form-group col-md-6">
-      <label for="linkedin">Linkedin</label>
-      <input type="text" class="form-control" value="<?=$data['linkedin']?>" name="linkedin" id="linkedin" placeholder="https://linkedin.com/MohanGo94273231">
+      <label for="twitch">Linkedin</label>
+      <input type="text" class="form-control" value="<?=$data['twitch']?>" name="twitch" id="twitch" placeholder="https://www.twitch.tv/killer_qu3en">
     </div>
     <div class="form-group col-md-6">
-      <label for="github">Github</label>
-      <input type="text" class="form-control"  value="<?=$data['github']?>" name="github" id="github" placeholder="https://github.com/MohanGo94273231">
+      <label for="spotify">Github</label>
+      <input type="text" class="form-control"  value="<?=$data['spotify']?>" name="spotify" id="spotify" placeholder="https://open.spotify.com/user/31j5wci3pc3bmg7hv6zgdsyrcapq  ">
     </div>
   </div>
   <div class="form-group">
     <label for="address">Address</label>
-    <input type="text" name="address" value="<?=$data['location']?>" class="form-control" id="address" placeholder="C-537 Mahavir Enclave Part-3, New Delhi 110059">
+    <input type="text" name="address" value="<?=$data['location']?>" class="form-control" id="address" placeholder="8/16/37/349 Minh Khai, Ha Noi, Viet Nam">
   </div>
   <div class="form-row">
   <div class="form-group col-md-9">
     <label for="profession">Proffesion Titles (Separate with ',' comma)</label>
-    <input type="text" class="form-control" name="profession" value="<?=$data['professions']?>" id="profession" placeholder="Web Developer,PHP Developer,Graphic Designer">
+    <input type="text" class="form-control" name="profession" value="<?=$data['professions']?>" id="profession" placeholder="Student">
   </div>
   <div class="form-group col-md-3">
     <label for="mobile">Mobile No</label>
-    <input type="text" class="form-control" value="<?=$data['mobile']?>" name="mobile" id="mobile" placeholder="+917838403916">
+    <input type="text" class="form-control" value="<?=$data['mobile']?>" name="mobile" id="mobile" placeholder="+84934342000">
   </div>
              </div>
   <input type="submit" name="save" class="btn btn-primary" value="Save Changes">
